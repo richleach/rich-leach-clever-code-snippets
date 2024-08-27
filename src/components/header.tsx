e689052adf0129323da7de2753a9e9bb73007a7d
Rich Leach <rich@cfsnap.com>
2024-08-27 15:15:59 -0600

    front/home page, setting up looks and copy

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
+
 export default function Header() {
     return (
-    <header className="w-full h-[100px] bg-gray-800 text-white flex items-center justify-center">
-        <h1>Header, my header</h1>
-    </header>
+        <div className="flex justify-between items-center">
+            <header
+                className="w-full h-[100px] bg-[url('/images/codeBackground.png')] text-white flex items-center justify-center">
+                <div className="w-1/3 ml-3">
+                    <Image src="/images/logo.png" alt="code snippets" height={80} width={80}/>
+                </div>
+
+
+                <div className="w-1/3 text-center text-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                    (mostly...) Clever Code Snippets
+                </div>
+
+
+                <div className="w-1/3 text-right mr-3">
+                    <p>&nbsp;</p>
+                </div>
+
+            </header>
+        </div>
     );
 }
-
