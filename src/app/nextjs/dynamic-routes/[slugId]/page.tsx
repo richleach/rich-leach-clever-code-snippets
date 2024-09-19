cde9c2872f32500f50c2a5be406f5de4e71e7531
Rich Leach <rich@cfsnap.com>
2024-09-18 18:00:15 -0600

    updates to Dynamic pages in Next js

--- src/app/nextjs/dynamic-routes/[slugId]/page.tsx
+++ src/app/nextjs/dynamic-routes/[slugId]/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-export default function ProductDetailsSample( {params} : { slugId:string } ) {
+export default function ProductDetailsSample( { params }: {params: {slugId:string}} ) {
     return (
         <>
-            <h1>You entered &quot;{params.slugId}&quot; as the dynamic slug value in the URL. Try entering a different number in the URL!</h1><br />
-
+            <h1>You entered &quot;{params.slugId}&quot; as the dynamic slug value in the URL. Try entering a different number in the URL!</h1>
+            <br />
             <Link href="/nextjs/dynamic-routes/">&lt;= Back to dynamic pages</Link>
 
         </>
